69d146a1e80875e86a70d2dd31862a5bc3666653
Knean <mario009c@yahoo.com>
2020-04-18 19:53:47 +0100

    dunno

--- tree/tasks.py
+++ tree/tasks.py
@@ -2,4 +2,4 @@ from celery import shared_task
 from .models import Tree
 @shared_task
 def buy(amount, user):
-    Tree.objects.first().buy(amount= amount, user= user) 
+    Tree.objects.first().buy(amount,1, user= user) 
