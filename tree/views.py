deb6d42cabdfdb99072216819a5e55d786a4b1e4
Knean <mario009c@yahoo.com>
2020-04-20 14:54:01 +0100

    api returns tree field

--- tree/views.py
+++ tree/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-from .models import Node
+from .models import Node, Tree
 from .serializers import NodeSerializer, UserSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -12,9 +12,12 @@ def node_list(request):
     List all code snippets, or create a new snippet.
     """
     if request.method == 'GET':
-        nodes = Node.objects.all()
-        serializer = NodeSerializer(nodes, many=True)
-        return Response(serializer.data)
+        #nodes = Node.objects.all()
+        #serializer = NodeSerializer(nodes, many=True)
+        tree = Tree.objects.first()
+        #return Response(serializer.data)
+        return Response(tree.json_string)
+
 
 @api_view(['GET'])
 def user_list(request):
