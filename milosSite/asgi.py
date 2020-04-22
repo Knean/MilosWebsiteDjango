fb1b74d0558721e539e1d38f8d94d4a8cdf07975
Knean <mario009c@yahoo.com>
2020-04-22 13:12:29 +0100

    get default application

--- milosSite/asgi.py
+++ milosSite/asgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 import os
 from channels.layers import get_channel_layer
 from django.core.asgi import get_asgi_application
-
+from channels.routing import get_default_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'milosSite.settings')
 
 application = get_default_application()
