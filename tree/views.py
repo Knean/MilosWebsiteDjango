0d753c5517e4ad476c0cb4f744da60fd67e32768
Knean <mario009c@yahoo.com>
2020-04-22 15:01:20 +0100

    empty string

--- tree/views.py
+++ tree/views.py
@@ -17,7 +17,7 @@ def node_list(request):
         #serializer = NodeSerializer(nodes, many=True)
         tree = Tree.objects.first()
         #return Response(serializer.data)
-        return Response(json.loads(tree.json_string or 'null'))
+        return Response(json.loads(tree.json_string or '{}'))
 
 
 @api_view(['GET'])
